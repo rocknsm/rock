d682a0762a4c9786591eaf94e03e686d7190418d
Jeff Geiger <jeff@jeffgeiger.com>
2015-12-16 13:47:05 -0600

    More CentOS/RHEL compatibility changes.

--- custom/cookbooks/simplerock/recipes/default.rb
+++ custom/cookbooks/simplerock/recipes/default.rb
@@ -139,6 +139,7 @@ end
 ##### Temp Fix - In prep for CentOS 7.2, a dependency of gperftools-lib (libunwind) was moved to the CR repo.
 execute 'enable_centos_cr' do
   command 'sed -i "s/enabled=0/enabled=1/g" /etc/yum.repos.d/CentOS-CR.repo'
+  only_if 'grep CentOS /etc/redhat-release'
 end
 
 fullver = node['kernel']['release']
