62a89ab3ae7d0d9c1b4480385f617ecc8cb7720a
Derek Ditch <derek@rocknsm.io>
2017-11-25 20:59:06 -0600

    Removed all chef cookbooks.

--- cookbooks/simplerock/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-## Tabled for now, opting to use dcode's java headless package.
-#node.normal['java']['install_flavor'] = "oracle_rpm"
-#node.normal['java']['oracle']['accept_oracle_download_terms'] = true
-#node.normal['java']['oracle_rpm']['package_version'] = "8"
-#node.normal['java']['oracle_rpm']['type'] = "jre"
-
