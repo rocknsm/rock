20cc3b08946a9b0466eedbb98f69e180cb848169
duckmanjbr <duckmanjbr@gmail.com>
2017-12-17 22:22:23 -0600

    deploy_rock.sh error check

--- bin/deploy_rock.sh
+++ bin/deploy_rock.sh
@@ -6,12 +6,21 @@ if [ "x${DEBUG}" != "x" ]; then
   VERBOSE_FLAGS="-vvv"
 fi
 if [[ ! -e /etc/rocknsm/config.yml ]];then
+        #Find script working dir
+        SOURCE="${BASH_SOURCE[0]}"
+        while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+           DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+           SOURCE="$(readlink "$SOURCE")"
+           # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located done
+           [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
         echo "[-] You must run generate_defaults.sh prior to deploying for the first time. "
         read -p "Would you like to generate the defaults now?  [y/n] " -n 1 -r
         if [[ $REPLY =~ ^[Yy]$ ]];then
                 echo ''
-                /bin/bash generate_defaults.sh
-                echo "Please verify configuration settings in /etc/rocknsm/config.yml before re-running the deploy script."
+                /bin/bash $DIR/generate_defaults.sh
+                echo "**** Please verify configuration settings in /etc/rocknsm/config.yml before re-running the deploy script."
                 sleep 3
                 exit
         else
