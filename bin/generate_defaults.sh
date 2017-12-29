eeb3af961021fc58535a93100ce390ede6d2fea0
Unknown <grantcurell@gmail.com>
2017-12-29 10:17:24 -0600

    Fixed a minor bug in generate defaults
    - There were missing fi statements in the bash logic.

--- bin/generate_defaults.sh
+++ bin/generate_defaults.sh
@@ -22,26 +22,26 @@ main() {
 	# Get the current directory of generate_defaults.sh (Ex: if deploy rock is in /root/rock/bin/generate_defaults.sh,
 	# this will return /root/rock/bin
 	SCRIPT_PATH=$(dirname $(readlink -f $0))
-	
+
 	# Returns rock's directory. Ex: If generate_defaults.sh is in /root/rock/bin then this returns /root/rock
 	TOPLEVEL=$(dirname ${SCRIPT_PATH})
 
 	cd "${TOPLEVEL}/playbooks"
-	
+
 	ansible-playbook "${TOPLEVEL}/playbooks/generate-defaults.yml" 2>&1 1>/dev/null
-	
+
 	# $? returns the return value of the last executed command. In this case, it will be the return value from
 	# ansible-playbook
 	retVal=$?
-	
+
 	# The return value of ansible-playbook will be non-zero if it failed for some reason.
 	if [ $retVal -ne 0 ]; then
-	
+
       echo "Dumping default variables failed! Verify you can run sudo without a password." 1>&2
   	  exit $retVal
-	
+
 	fi
-	
+
 	echo "Defaults generated. Adjust /etc/rocknsm/config.yml as needed."
 }
 
@@ -57,13 +57,15 @@ check_for_config_file(){
 		echo ""
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
 			return
-			
+
 		else
 			echo "Exiting..."
 			exit 0
 		#Alternatively we can loop if the user does not actually answer Y|y|N|n.
 		#E.g. Please answer 'y' to continue or 'n' to quit.
 		#$_
+	  fi
+	fi
 }
 
 check_for_config_file
