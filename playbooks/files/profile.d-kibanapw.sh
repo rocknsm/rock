797b59d9e79b86d92334dda80e3b2a0ec1e19aad
Unknown <grantcurell@gmail.com>
2018-01-05 06:54:44 -0600

    Initial Commit

--- playbooks/files/profile.d-kibanapw.sh
+++
@@ -1,8 +0,0 @@
-# Set passwords
-function kibanapw() { if [ $# -lt 2 ]; then echo -e "Usage: kibanapw USER PASSWORD\nUsers will be added to /etc/nginx/.htpasswd"; else egrep "^${1}:" /etc/nginx/.htpasswd > /dev/null 2>&1; if [[ $? -eq 0 ]]; then sudo sed -i "/${1}\:/d" /etc/nginx/.htpasswd; fi; printf "${1}:$(echo ${2} | openssl passwd -apr1 -stdin)\n" | sudo tee -a /etc/nginx/.htpasswd > /dev/null 2>&1; fi; }
-
-# Enable Auth
-function enable_kibana_auth() { sudo sed -i 's/#auth_basic/auth_basic/g' /etc/nginx/conf.d/rock.conf; }
-
-# Disable Auth
-function disable_kibana_auth() { sudo sed -i 's/auth_basic/#auth_basic/g' /etc/nginx/conf.d/rock.conf; }
