90e61f74fc5029562a7316fc4c07a5c5be701e57
anlx-sw <anlx-sw@users.noreply.github.com>
2018-02-23 22:39:19 +0100

    shell script to reset rock_data to "fresh" state (#245)

---
+++ bin/reset_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+TOPLEVEL=$(dirname ${SCRIPT_PATH})
+VERBOSE_FLAGS=
+if [ "x${DEBUG}" != "x" ]; then
+  VERBOSE_FLAGS="-vvv"
+fi
+
+
+read -p "Do you really want to delete all rock data? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+echo "Stopping Rock Services"
+
+
+cd ${TOPLEVEL}/playbooks
+ansible-playbook "${TOPLEVEL}/playbooks/delete-data.yml" ${VERBOSE_FLAGS}
+ansible-playbook "${TOPLEVEL}/playbooks/deploy-rock.yml" ${VERBOSE_FLAGS}
+
+/sbin/rock_start
+fi
